500b5b7903b7d62a36480797bc14a501923f939d
Andrew <70863018+Andss-ye@users.noreply.github.com>
2025-08-25 18:36:50 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use('/books/', bookRoutes)
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor en http://localhost:${PORT}`)
